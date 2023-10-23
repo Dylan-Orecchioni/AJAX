c36595f6a6ecfe1ad595b28b8b380e4c1e89d9f4
Dylan <dylan77120@live.fr>
2023-10-23 16:59:19 +0200

    TP2 ajax

--- script.js
+++ script.js
@@ -2,13 +2,31 @@
 
 const btn = document.querySelector('.button');
 const img = document.querySelector('.box img');
-console.log(btn);
-console.log(img);
+const select = document.querySelector('#race');
+
+fetch('https://dog.ceo/api/breeds/list/all')
+.then(res => res.json())
+.then(data => {
+    const breeds = data.message;
+
+    Object.keys(breeds).forEach(breed => {
+        const option = document.createElement('option');
+        option.value = breed;
+        option.textContent = breed;
+        select.appendChild(option);
+    })
+})
 
 btn.addEventListener('click', () => {
-    fetch('https://dog.ceo/api/breed/husky/images/random')
+    const race = select.value;
+
+    if(race){
+        fetch(`https://dog.ceo/api/breed/${race}/images/random`)
         .then(res => res.json())
         .then(data => {
             img.src = data.message;
         })
+    }
 })
+
+
